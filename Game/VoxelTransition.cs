682ed357a3d27a5f8b0e22f5ff780d5bc5ec0ae9
Jack Fallows <JackFallows@users.noreply.github.com>
2020-08-26 15:08:39 +0100

    cosine interpolation

--- Game/VoxelTransition.cs
+++ Game/VoxelTransition.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq.Expressions;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -12,8 +11,6 @@ namespace TextureMorph
 
         private TimeSpan? startTime = null;
         private TimeSpan? endTime = null;
-        private Vector2 positionSpeed;
-        private Vector3 colorSpeed;
 
         private Voxel active;
 
@@ -36,24 +33,6 @@ namespace TextureMorph
 
             startTime = currentGameTime.TotalGameTime;
             endTime = startTime.Value.Add(TimeSpan.FromSeconds(durationSeconds));
-
-            var xSpeed = Math.Abs(source.Position.X - target.Position.X) / (durationSeconds * 1000);
-            var ySpeed = Math.Abs(source.Position.Y - target.Position.Y) / (durationSeconds * 1000);
-
-            xSpeed = source.Position.X >= target.Position.X ? xSpeed * -1.0f : xSpeed;
-            ySpeed = source.Position.Y >= target.Position.Y ? ySpeed * -1.0f : ySpeed;
-
-            positionSpeed = new Vector2(xSpeed, ySpeed);
-
-            var rSpeed = Math.Abs(source.Color.R - target.Color.R) / (durationSeconds * 1000f);
-            var gSpeed = Math.Abs(source.Color.G - target.Color.G) / (durationSeconds * 1000f);
-            var bSpeed = Math.Abs(source.Color.B - target.Color.B) / (durationSeconds * 1000f);
-
-            rSpeed = source.Color.R >= target.Color.R ? rSpeed * -1.0f : rSpeed;
-            gSpeed = source.Color.G >= target.Color.G ? gSpeed * -1.0f : gSpeed;
-            bSpeed = source.Color.B >= target.Color.B ? bSpeed * -1.0f : bSpeed;
-
-            colorSpeed = new Vector3(rSpeed, gSpeed, bSpeed);
         }
 
         public void Update(GameTime gameTime)
@@ -69,18 +48,17 @@ namespace TextureMorph
                 return;
             }
 
-            var elapsed = gameTime.TotalGameTime - startTime.Value;
-            var elapsedMilliseconds = elapsed.TotalMilliseconds;
+            var mu = GetMu(startTime.Value.TotalMilliseconds, endTime.Value.TotalMilliseconds, gameTime.TotalGameTime.TotalMilliseconds);
 
-            var xDiff = (float)(positionSpeed.X * elapsedMilliseconds);
-            var yDiff = (float)(positionSpeed.Y * elapsedMilliseconds);
+            var x = CosineInterpolate(source.Position.X, target.Position.X, mu);
+            var y = CosineInterpolate(source.Position.Y, target.Position.Y, mu);
+            
+            var r = CosineInterpolate(source.Color.R, target.Color.R, mu);
+            var g = CosineInterpolate(source.Color.G, target.Color.G, mu);
+            var b = CosineInterpolate(source.Color.B, target.Color.B, mu);
 
-            var rDiff = (float)(colorSpeed.X * elapsedMilliseconds);
-            var gDiff = (float)(colorSpeed.Y * elapsedMilliseconds);
-            var bDiff = (float)(colorSpeed.Z * elapsedMilliseconds);
-
-            active.Position = new Vector2(source.Position.X + xDiff, source.Position.Y + yDiff);
-            active.Color = new Color((int)Math.Floor(source.Color.R + rDiff), (int)Math.Floor(source.Color.G + gDiff), (int)Math.Floor(source.Color.B + bDiff));
+            active.Position = new Vector2((float)x, (float)y);
+            active.Color = new Color((int)r, (int)g, (int)b);
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
@@ -93,6 +71,26 @@ namespace TextureMorph
             {
                 target.Draw(gameTime, spriteBatch);
             }
+        }
+
+        // http://paulbourke.net/miscellaneous/interpolation/
+        private static double LinearInterpolate(double v1, double v2, double mu)
+        {
+            return (v1 * (1 - mu) + v2 * mu);
+        }
+
+        private static double CosineInterpolate(double v1, double v2, double mu)
+        {
+            double mu2;
+
+            mu2 = (1 - Math.Cos(mu * Math.PI)) / 2;
+            return (v1 * (1 - mu2) + v2 * mu2);
+        }
+
+        // https://stats.stackexchange.com/a/154211
+        private static double GetMu(double startMilliseconds, double endMilliseconds, double currentMilliseconds)
+        {
+            return (currentMilliseconds - startMilliseconds) / (endMilliseconds - startMilliseconds);
         }
     }
 }
